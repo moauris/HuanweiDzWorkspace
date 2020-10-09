f51c4b42641be03d3607cd57382de9df253b0d36
unknown <mchenf@icloud.com>
2020-10-10 02:49:56 +0800

    First Commit

---
+++ HuanweiDzModels/LedgerItem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection;
+
+namespace HuanweiDzModels
+{
+    public class LedgerItem
+    {
+        #region Constructor
+        public LedgerItem(LedgerSides side
+            , DateTime date, string info, double credit
+            , double debit, string direction
+            , double remain)
+        {
+            Side = side;
+            DateIncured = date;
+            Info = info; Credit = credit; Debit = debit; Direction = direction;
+            RemainingFund = remain;
+        }
+        public LedgerItem(LedgerSides side, object[] parameters)
+        {
+            //判定是否正好具有6个元素
+            if (parameters.Length != 6) throw new TargetParameterCountException("试图生成的参数数量不正确。");
+            Side = side;
+            if (parameters[0] is null)
+            {
+                DateIncured = null;
+            }
+            else
+            {
+                DateIncured = (DateTime)parameters[0];
+            }
+            Info = (string)parameters[1]; 
+            Credit = (double)parameters[2]; 
+            Debit = (double)parameters[3];
+            Direction = (string)parameters[4]; ;
+            RemainingFund = (double)parameters[5];
+        }
+        #endregion
+
+        #region Properties
+        public LedgerSides Side { get; set; }
+        public DateTime? DateIncured { get; set; }
+        public string IncuredOn
+        { 
+            get
+            {
+                if (DateIncured is null) return "未知";
+                return ((DateTime)DateIncured).ToString("yyyy年MM月dd日");
+            }
+        }
+       
+        public string Info { get; set; }
+        public double Credit { get; set; }
+        public double Debit { get; set; }
+        public string Direction { get; set; }
+        public double RemainingFund { get; set; }
+        #endregion
+
+        #region Methods
+        public static bool IsValid(object[] Items)
+        {
+            //TODO: Make new Validation method for LedgerItem 
+            throw new NotImplementedException();
+        }
+        public static LedgerItem BuildIfValid(LedgerSides side, object[] Items)
+        {
+            if (IsValid(Items)) return new LedgerItem(side, Items);
+            return null;
+        }
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+
+    public enum LedgerSides
+    {
+        FromCompany, FromBank
+    }
+}
---
+++ HuanweiDzWorkspace/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HuanweiDzModels;
+
+namespace HuanweiDzWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            object[] parameters =
+            {
+                null, //new DateTime(1993, 3, 27),
+                "测试用的 Ledger 对象",
+                0D,
+                3997D,
+                "贷方",
+                322209.27D
+            };
+            LedgerItem ledger = new LedgerItem(LedgerSides.FromBank, parameters);
+
+            Console.WriteLine(ledger.Credit);
+            Console.WriteLine(ledger.Debit);
+            Console.WriteLine(ledger.IncuredOn);
+            Console.WriteLine(ledger.Info);
+
+        }
+    }
+}
